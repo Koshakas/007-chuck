3b97324ff1d47c5a9af772167d0371a58201620b
Konstantinas Jariomenko <k.jariomenko@gmail.com>
2021-11-24 19:56:31 +0200

    first commit

---
+++ src/components/Jokes.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Jokes extends React.Component {
+    render() {
+        return (
+            <div className="jokes">
+                <h1>Soon</h1>
+            </div>
+        );
+    }
+}
+
+export default Jokes;
