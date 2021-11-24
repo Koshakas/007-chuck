9d02eaa90485974edcfc869df2629cd49eba3a7e
Konstantinas Jariomenko <k.jariomenko@gmail.com>
2021-11-24 20:42:02 +0200

    fetch Jokes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Jokes from "./components/Jokes";
+import Favorites from "./components/Favorites";
+import Nav from "./components/Nav";
 
 class App extends React.Component {
     render() {
-        return <div className="container"></div>;
+        return (
+            <div className="container">
+                <Router>
+                    <Nav />
+                    <Routes>
+                        <Route path="/" element={<Jokes />} />
+                        <Route path="/favorites" element={<Favorites />} />
+                    </Routes>
+                </Router>
+            </div>
+        );
     }
 }
 
--- src/components/Jokes.jsx
+++ src/components/Jokes.jsx
@@ -1,12 +1,38 @@
 import React from "react";
+import Joke from "./Joke";
 
 class Jokes extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            jokes: []
+        };
+    }
+
+    componentDidMount() {
+        fetch("http://api.icndb.com/jokes/id/1")
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ jokes: data.value });
+            });
+    }
+
     render() {
-        return (
-            <div className="jokes">
-                <h1>Soon</h1>
-            </div>
-        );
+        if (this.state.jokes.length) {
+            return (
+                <div className="jokes">
+                    {this.state.jokes.map(j => (
+                        <Joke id={j.id} key={j.id} joke={j.joke} />
+                    ))}
+                </div>
+            );
+        } else {
+            return (
+                <div className="d-flex justify-content-center">
+                    <div className="spinner-border" role="status"></div>
+                </div>
+            );
+        }
     }
 }
 
